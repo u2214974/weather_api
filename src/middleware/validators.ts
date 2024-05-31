ebdf75843980c5fa6bd44a020af925ce34185565
u2214974 <162577809+u2214974@users.noreply.github.com>
2024-05-31 02:30:38 +0000

    updated movies name

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -1,5 +1,8 @@
 import { param  } from "express-validator";
 
+
+
+const allowedGenres = ["action", "comedy", "drama", "horror", "thriller", "romance", "sci-fi", "fantasy"];
 /**
  * Validates the genre query param
  * @returns {ValidationChain} - Express validator validation chain
@@ -14,6 +17,6 @@ export const validateGenre = param("genre")
   // We will use the isString method to check if the genre param is a string
   .isString()
   // Optionally, you can check if the genre is one of the allowed genres
-  .isIn(["Action", "Comedy", "Drama", "Horror", "Thriller", "Romance", "Sci-Fi", "Fantasy"])
+  .custom(value => allowedGenres.includes(value.toLowerCase()))
   // with message method to genrate custom error 
   .withMessage("Genre must be one of the following: Action, Comedy, Drama, Horror, Thriller, Romance, Sci-Fi, Fantasy");
--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,12 +1,33 @@
 import { faker } from "@faker-js/faker";
 
 
+// a list of movie name since random generation generated wierd names 
+
+const movieTitles = [
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Dark Knight",
+  "Pulp Fiction",
+  "Forrest Gump",
+  "Inception",
+  "Fight Club",
+  "The Matrix",
+  "Goodfellas",
+  "The Lord of the Rings",
+  "Star Wars",
+  "Jurassic Park",
+  "Titanic",
+  "The Lion King",
+  "Back to the Future"
+];
+const getRandomItem = (arr: string[]): string => arr[Math.floor(Math.random() * arr.length)];
+
 // below function generates a fake data for the movie interface based on genre which includes #title # releaseDate #director and #rating
 export const generateMovieData = (genre:String): MovieData => {
   // Generate random weather data
   const generatedMovieData = {
     // title of the movies 
-    title: faker.lorem.words(3),
+    title: getRandomItem(movieTitles),
     //release date of the movie 
     releaseDate: faker.date.past(20).toISOString().split('T')[0], // Generating a past date within the last 20 years
      genre:[genre],

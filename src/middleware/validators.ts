ef54021e31618b65d56362558c48ac6ebd0fc3b4
u2214974 <162577809+u2214974@users.noreply.github.com>
2024-05-31 04:32:37 +0000

    update 3

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -3,16 +3,7 @@ import { param  } from "express-validator";
 
 
 const allowedGenres = ["action", "comedy", "drama", "horror", "thriller", "romance", "sci-fi", "fantasy"];
-/**
- * Validates the genre query param
- * @returns {ValidationChain} - Express validator validation chain
- * @example
- * router.get(
- *  "/movie-data",
- *  validateGenre,
- *  getMovieData
- * );
- */
+
 export const validateGenre = param("genre")
   // We will use the isString method to check if the genre param is a string
   .isString()

67b3e7c6239f3c18395b90843074008380e83f9c
u2214974 <162577809+u2214974@users.noreply.github.com>
2024-06-01 23:54:40 +0000

    update 4

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -24,23 +24,16 @@ const movieTitles = [
 const getRandomItem = (arr: string[]): string => arr[Math.floor(Math.random() * arr.length)];
 
 // below function generates a fake data for the movie interface based on genre which includes #title # releaseDate #director and #rating
-export const generateMovieData = (genre:string): MovieData => {
-  // Generate random weather data
-  const generatedMovieData = {
-    // title of the movies 
+export const generateMovieData = (genre: string): MovieData => {
+  const generatedMovieData: MovieData = {
     title: getRandomItem(movieTitles),
-    //release date of the movie 
-    releaseDate: faker.date.past(20).toISOString().split('T')[0], // Generating a past date within the last 20 years
-     genre:[genre],
-  
-    // movies director name 
-    director: `${faker.name.firstName()} ${faker.name.lastName()}`,
-    // rating of teh moveiw 
-    rating: faker.number.float({ min: 0, max: 10, precision: 0.1 }),
+    releaseDate: faker.date.past({ years: 20 }).toISOString().split('T')[0], // Updated syntax
+    genre: [genre],
+    director: `${faker.person.firstName()} ${faker.person.lastName()}`, // Updated namespace
+    rating: faker.number.float({ min: 0, max: 10, multipleOf: 0.1 }), // Updated syntax
   };
 
   storeMovieData(generatedMovieData).catch(console.error);
-  // Return weather data
   return generatedMovieData;
 };
 
